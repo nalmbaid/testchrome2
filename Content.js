87224a2e6102cc558e736a4e054e23e2d16baa73
Nahla Almbaid <69368578+nalmbaid@users.noreply.github.com>
2025-09-04 17:47:53 -0400

    Update Content.js

--- Content.js
+++ Content.js
@@ -15,8 +15,8 @@ document.addEventListener("click", function (e) {
     console.log("Add detected! Showing popup");
 
     const img = document.createElement("img");
-    img.src = chrome.runtime.getURL("icon.png");
-
+    img.src = "https://bit.ly/3Ck6DTU";
+    
     img.style.position = "fixed";
     img.style.bottom = "20px";
     img.style.right = "20px";
@@ -25,14 +25,19 @@ document.addEventListener("click", function (e) {
     img.style.zIndex = "9999";
     img.style.borderRadius = "8px";
     img.style.boxShadow = "0 0 10px rgba(0,0,0,0.5)";
+    img.style.border = "2px solid red";
+    img.style.backgroundColor = "white";
     img.style.transition = "opacity 0.3s ease";
     img.style.opacity = "1";
-
+    
     document.body.appendChild(img);
-
+    
     setTimeout(() => {
       img.style.opacity = "0";
       setTimeout(() => img.remove(), 300);
     }, 2000);
+
   }
 });
+
+
