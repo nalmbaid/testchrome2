6e5e1cc3f2ba401d4e048dc42502399248f03ff2
Nahla Almbaid <69368578+nalmbaid@users.noreply.github.com>
2025-09-04 18:12:47 -0400

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,18 +1,12 @@
-console.log("Content script loaded");
-
 document.addEventListener("click", function (e) {
-  console.log("Clicked element:", e.target);
 
   const button = e.target.closest("button");
-  console.log("Closest button:", button);
 
   if (button) {
     const text = button.textContent.toLowerCase();
-    console.log("Button text:", text);
   }
 
   if (button && button.textContent.toLowerCase().includes("add")) {
-    console.log("Add detected! Showing popup");
 
     const img = document.createElement("img");
     img.src = "https://bit.ly/3Ck6DTU";
@@ -20,23 +14,18 @@ document.addEventListener("click", function (e) {
     img.style.position = "fixed";
     img.style.bottom = "20px";
     img.style.right = "20px";
-    img.style.width = "50px";
-    img.style.height = "50px";
+    img.style.width = "500px";
+    img.style.height = "500px";
     img.style.zIndex = "9999";
     img.style.borderRadius = "8px";
     img.style.boxShadow = "0 0 10px rgba(0,0,0,0.5)";
     img.style.border = "2px solid red";
-    img.style.backgroundColor = "white";
     img.style.transition = "opacity 0.3s ease";
     img.style.opacity = "1";
-    
+
+    ///https://www.w3schools.com/jsref/met_node_appendchild.asp
     document.body.appendChild(img);
     
-    setTimeout(() => {
-      img.style.opacity = "0";
-      setTimeout(() => img.remove(), 300);
-    }, 2000);
-
   }
 });
 
